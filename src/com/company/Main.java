4fe52c66755879cd118f57002957eed218888370
Vasily Gudin <gudin@list.ru>
2022-05-16 23:09:48 +0300

    Добавлено в GitHub

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Calculator calc = Calculator.instance.get();
+        int a = (int) calc.plus.apply(1, 2);
+        int b = (int) calc.minus.apply(1,1);
+        String c = (String) calc.devide.apply(String.valueOf(a), String.valueOf(b)); // Возможна ошибка деления на ноль
+
+        calc.println.accept(c);
+
     }
 }
